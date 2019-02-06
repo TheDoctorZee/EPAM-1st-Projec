4a3660dec252c37675d2c795ea91384c5bee15ea
Ilia_Stepanov <Ilia_Stepanov@epam.com>
2019-02-06 20:55:38 +0300

    simple DB init from resource file

--- src/main/java/com/epam/lowcost/db/DBinitialize.java
+++ src/main/java/com/epam/lowcost/db/DBinitialize.java
@@ -3,10 +3,10 @@ package com.epam.lowcost.db;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.*;
 
 public class DBinitialize {
 
@@ -18,64 +18,37 @@ public class DBinitialize {
         this.basicDataSource = basicDataSource;
         conn = basicDataSource.getConnection();
         stm = conn.createStatement();
-        createTable();
-        populateTable();
-        selectFromTable();
+        initiateDB();
+
 
     }
 
 
+    public void initiateDB() throws SQLException {
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("C:\\Users\\Ilia_Stepanov\\IdeaProjects\\EPAM-1st-Projec\\src\\main\\resources\\createTable"))) {
 
-    public void createTable() throws SQLException {
-
-        String sql =  "CREATE TABLE   REGISTRATION " +
-                "(id INTEGER not NULL, " +
-                " first VARCHAR(255), " +
-                " last VARCHAR(255), " +
-                " age INTEGER, " +
-                " PRIMARY KEY ( id ))";
-
-        stm.executeUpdate(sql);
-
-    }
-    public void populateTable() throws SQLException{
-        String sql = "INSERT INTO Registration " + "VALUES (100, 'Zara', 'Ali', 18)";
-
-        stm.executeUpdate(sql);
-        sql = "INSERT INTO Registration " + "VALUES (101, 'Mahnaz', 'Fatma', 25)";
-
-        stm.executeUpdate(sql);
-        sql = "INSERT INTO Registration " + "VALUES (102, 'Zaid', 'Khan', 30)";
-
-        stm.executeUpdate(sql);
-        sql = "INSERT INTO Registration " + "VALUES(103, 'Sumit', 'Mittal', 28)";
-
-        stm.executeUpdate(sql);
-        System.out.println("Inserted records into the table...");
-
-    }
-
-
-    private void selectFromTable() throws SQLException {
-        String sql = "SELECT * from REGISTRATION";
-        ResultSet resultSet = stm.executeQuery(sql);
-            while (resultSet.next())
-            {
-                int id  = resultSet.getInt("id");
-                int age = resultSet.getInt("age");
-                String first = resultSet.getString("first");
-                String last = resultSet.getString("last");
-
-                // Display values
-                System.out.print("ID: " + id);
-                System.out.print(", Age: " + age);
-                System.out.print(", First: " + first);
-                System.out.println(", Last: " + last);
-
+            String sql = br.readLine();
+            while (sql != null) {
+                stm.addBatch(sql);
+                sql = br.readLine();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stm.executeBatch();
+        ResultSet rs = stm.executeQuery("SELECT * FROM REGISTRATION");
+        ResultSetMetaData rsm = rs.getMetaData();
+        int columnsNumber = rsm.getColumnCount() + 1;
+        while (rs.next()) {
+            for (int i = 1; i < columnsNumber; i++) {
+                System.out.print(" " + rs.getMetaData().getColumnName(i) + "=" + rs.getObject(i));
+            }
+            System.out.println();
+        }
 
 
     }
+
 
 }
 
