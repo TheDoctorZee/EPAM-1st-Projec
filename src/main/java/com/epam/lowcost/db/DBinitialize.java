a7b182d0b86ffd485d4f81ca38b5ffe3c82310e2
Ilia Stepanov <47216173+IliaStepanov@users.noreply.github.com>
2019-02-07 14:31:02 +0300

    Update DBinitialize.java
    Moved Connection and Statement  open\close  from constructor to method. Removed printAllDb method.

--- src/main/java/com/epam/lowcost/db/DBinitialize.java
+++ src/main/java/com/epam/lowcost/db/DBinitialize.java
@@ -17,11 +17,30 @@ public class DBinitialize {
     private Statement stm;
 
     public DBinitialize(BasicDataSource basicDataSource) throws SQLException {
+        this.basicDataSource = basicDataSource;
+        initiateDB();
+    }
+
+
+     private void initiateDB() {
+        File fl = new File(this.getClass().getResource("/createTable").getFile());
+        List<String> sqlBatch = new ArrayList<>();
         try {
             conn = basicDataSource.getConnection();
             stm = conn.createStatement();
-            initiateDB();
-            printAllDb();
+            try (Stream<String> str = Files.lines(fl.toPath())) {
+                str.forEach(sqlBatch::add);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                for (String s : sqlBatch) {
+                    stm.addBatch(s);
+                }
+                stm.executeBatch();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
@@ -37,54 +56,6 @@ public class DBinitialize {
             }
         }
     }
-
-
-    private void initiateDB() throws SQLException {
-        File fl = new File(this.getClass().getResource("/createTable").getFile());
-        List<String> sqlBatch = new ArrayList<>();
-        try (Stream<String> str = Files.lines(fl.toPath())) {
-            str.forEach(sqlBatch::add);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        for (String s : sqlBatch) {
-            stm.addBatch(s);
-        }
-
-        stm.executeBatch();
-    }
-
-    private void printAllDb() {
-        List<String> tables = new ArrayList<>();
-        try {
-            DatabaseMetaData dm = conn.getMetaData();
-            ResultSet rs = dm.getTables(null, null, "%", null);
-            while (rs.next()) {
-                if (rs.getString(4).equalsIgnoreCase("TABLE")) {
-                    tables.add(rs.getString(3));
-                }
-            }
-            ResultSetMetaData rsm;
-            for (String s : tables) {
-                System.out.println(String.format("=================%s==============", s));
-                rs = stm.executeQuery("SELECT * FROM " + s);
-                rsm = rs.getMetaData();
-                int columns = rsm.getColumnCount() + 1;
-
-                while (rs.next()) {
-                    for (int i = 1; i < columns; i++) {
-                        System.out.print(" " + rs.getMetaData().getColumnName(i) + "=" + rs.getObject(i));
-                    }
-                    System.out.println();
-                }
-                System.out.println();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
-
 }
 
 
